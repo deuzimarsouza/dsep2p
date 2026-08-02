6ddfbfe9227d4c93ca649f88b7f8a155c8adaa8e
Deuzimar Souza <deuz.i.mar@outlook.com>
2026-08-02 04:16:08 -0300

    Remove trailing slash from apiBaseUrl

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PONTE_CONFIG = Object.freeze({
   // O workflow do GitHub Pages substitui este valor pela variável PONTE_API_URL.
   // Também é possível configurar o endereço pelo menu da própria página.
-  apiBaseUrl: "https://dsep2p-production.up.railway.app/",
+  apiBaseUrl: "https://dsep2p-production.up.railway.app",
 });
